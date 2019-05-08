c50927f09f192ea2807f6dcd82b0cebbbd50e3b5
Lovdeep Singh <lovdeep7@gmail.com>
2019-05-08 15:54:52 -0700

    Base realtime geotracking Android app

---
+++ README.md
@@ -0,0 +1,45 @@
+# Self Driving Car App
+
+
+[Click here for full tutorial]()
+
+
+# Quickstart
+
+1. In order to set up PubNub in your android app, you will need to create a PubNub app in the PubNub Admin Dashboard (it’s free). Upon creating the app, you will be assigned a subscribe key and publish key. You will need to go to the Constants.java class and enter these credentials where it says INSERT_SUBSCRIBE_KEY and INSERT_PUBLISH_KEY respectively.
+
+2. Now, we must set up our Google Maps API key, so that we can use it in our application. In order to do this head over to Google’s Developer API console. Here, we will create a new project and then generate an API key. Once you have the API Key, enter it in the Android Manifest file, where it says INSERT_API_KEY.
+
+3. Ensure that you have downloaded Google Play Services in your Android SDK Manager, accessible from the tool bar at the top of Android Studio. 
+
+# Required Gradle Dependencies
+
+``` 
+implementation group: 'com.pubnub', name: 'pubnub-gson', version: '4.12.0'
+
+implementation 'com.google.android.gms:play-services-maps:15.0.1'
+
+implementation "com.google.android.gms:play-services-location:15.0.1"
+
+implementation group: 'com.fasterxml.jackson.core', name: 'jackson-core', version: '2.9.2'
+
+implementation group: 'com.fasterxml.jackson.core', name: 'jackson-annotations', version: '2.9.2'
+
+implementation group: 'com.fasterxml.jackson.core', name: 'jackson-databind', version: '2.9.2'
+```
+
+# Required Manifest Permissions
+
+``` xml
+    <uses-permission android:name="android.permission.INTERNET" />
+    <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+    <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+    <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
+```
+
+**Sign up for PubNub click here:**
+
+<a href="https://dashboard.pubnub.com/signup?devrel_gh=RealtimeTaxiAndroidDemo">
+    <img alt="PubNub Signup" src="https://i.imgur.com/og5DDjf.png" width=260 height=97/>
+</a>
+
---
+++ app/src/main/java/com/pubnub/selfdrivingdemo/MainActivity.java
@@ -0,0 +1,83 @@
+package com.pubnub.selfdrivingdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
+
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+import com.pubnub.kaushik.realtimetaxiandroiddemo.util.Constants;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static PubNub pubnub; // Pubnub instance
+
+    Button driverButton, passengerButton; // Buttons that redirect user to proper view
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        driverButton = (Button) findViewById(R.id.driverButton);
+        passengerButton = (Button) findViewById(R.id.passengerButton);
+
+        initPubnub();
+
+        // Send user to Driver Activity or Passenger Activity using intents
+        driverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DriverActivity.class));
+            }
+        });
+        passengerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PassengerActivity.class));
+            }
+        });
+
+
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
+            checkPermission();
+        }
+
+    }
+
+    /*
+        Creates PNConfiguration instance and enters Pubnub credentials to create Pubnub instance.
+        This Pubnub instance will be used whenever we need to create connection to Pubnub.
+     */
+    private void initPubnub() {
+        PNConfiguration pnConfiguration = new PNConfiguration();
+        pnConfiguration.setSubscribeKey(Constants.PUBNUB_SUBSCRIBE_KEY);
+        pnConfiguration.setPublishKey(Constants.PUBNUB_PUBLISH_KEY);
+        pnConfiguration.setSecure(true);
+        pubnub = new PubNub(pnConfiguration);
+    }
+
+    /*
+        Checks user's location permission to see whether user has granted access to fine location and coarse location.
+        If not it will request these permissions.
+     */
+    public void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                ) {//Can add more as per requirement
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    123);
+        }
+    }
+
+}
